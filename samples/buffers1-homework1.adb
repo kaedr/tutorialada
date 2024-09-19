05870e0fe8083760d3231b4aa569c822c997c9f9
Henry Corse <kaedraar@gmail.com>
2024-09-19 15:54:15 -0400

    Got loop changed to include array slice copying, still no proof

--- samples/buffers1-homework1.adb
+++ samples/buffers1-homework1.adb
@@ -8,20 +8,18 @@ package body Buffers1.Homework1 is
       Next, Temp : Character;
    begin
       Output := (others => ' ');
-      for InputIndex in 1 .. Input'Length loop
+      for InputIndex in Input'Range loop
          Next := Input (InputIndex);
          for OutputIndex in 1 .. InputIndex loop
             if InputIndex = OutputIndex then
                Output (OutputIndex) := Next;
-            elsif Next < Output (OutputIndex) then
-               Temp                 := Output (OutputIndex);
+            elsif Next > Output (OutputIndex) then
+               -- Shift the remaining elements to the right
+               Output(OutputIndex + 1 .. InputIndex) := Output(OutputIndex .. InputIndex - 1);
                Output (OutputIndex) := Next;
-               Next                 := Temp;
+               exit;
             end if;
-            pragma Loop_Invariant
-              (OutputIndex <= InputIndex and
-               (Output (OutputIndex) = Input (InputIndex) or
-                Next = Input (InputIndex)));
+            pragma Loop_Invariant (for some J in 1..InputIndex => Output (OutputIndex) = Input(J));
          end loop;
          pragma Loop_Invariant
            (for some L in 1 .. InputIndex => Output (L) = Input (InputIndex));
