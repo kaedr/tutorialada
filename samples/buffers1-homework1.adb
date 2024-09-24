b38a02e14c7ccbc3660d1d8fe369fe63c3375d7f
Henry Corse <kaedraar@gmail.com>
2024-09-24 15:46:59 -0400

    Work on proofs

--- samples/buffers1-homework1.adb
+++ samples/buffers1-homework1.adb
@@ -15,14 +15,21 @@ package body Buffers1.Homework1 is
                Output (OutputIndex) := Next;
             elsif Next > Output (OutputIndex) then
                -- Shift the remaining elements to the right
-               Output(OutputIndex + 1 .. InputIndex) := Output(OutputIndex .. InputIndex - 1);
-               Output (OutputIndex) := Next;
+               Output (OutputIndex + 1 .. InputIndex) :=
+                 Output (OutputIndex .. InputIndex - 1);
+               Output (OutputIndex)                   := Next;
                exit;
             end if;
-            pragma Loop_Invariant (for some J in 1..InputIndex => Output (OutputIndex) = Input(J));
+            pragma Loop_Invariant
+              (for all I in Input'First .. InputIndex =>
+                 (for some J in Input'First .. InputIndex =>
+                      Output (I) = Input (J)));
+            pragma Loop_Invariant (Sorted(Output, OutputIndex));
          end loop;
          pragma Loop_Invariant
-           (for some L in 1 .. InputIndex => Output (L) = Input (InputIndex));
+           (for all I in Input'First .. InputIndex =>
+              (for some J in Input'First .. InputIndex =>
+                 Output (I) = Input (J)));
       end loop;
    end Immutable_Sort;
 
--- samples/buffers1-homework1.ads
+++ samples/buffers1-homework1.ads
@@ -1,11 +1,22 @@
 pragma SPARK_Mode (On);
+with Ada;
+with Ada.Characters.Latin_1;
 
 package Buffers1.Homework1 is
+   function Sorted
+     (BT : Buffer_Type; Stop : Buffer_Index_Type) return Boolean is
+     (if Stop > 1 then
+        (for all I in BT'First + 1 .. Stop => BT (I - 1) >= BT (I))) with
+     Ghost;
+
    procedure Immutable_Sort
      (Input : in Buffer_Type; Output : out Buffer_Type) with
+     Pre  =>
+      (for all I in Input'Range => Input (I) /= Ada.Characters.Latin_1.NUL),
      Post =>
       (for all I in Output'Range =>
-         (for some J in Input'Range => Output (I) = Input (J))); --and
-      --(for all I in Input'Range =>
-         --(for some J in Output'Range => Input (I) = Output (J)));
+         (for some J in Input'Range => Output (I) = Input (J))) and
+      (for all I in Input'Range =>
+         (for some J in Output'Range => Input (I) = Output (J))) and
+      (Sorted (Output, Output'Last));
 end Buffers1.Homework1;
